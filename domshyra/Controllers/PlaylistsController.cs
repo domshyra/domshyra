a846f7c05195961ea8db03102bcf324c20f75839
Dominic Schira <dominic.schira@curriebrown.com>
2019-12-03 11:05:56 -0800

    basic set up of playlist page

---
+++ domshyra/Controllers/PlaylistsController.cs
@@ -0,0 +1,25 @@
+﻿using domshyra.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace domshyra.Controllers
+{
+    public class PlaylistsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //private List<PlaylistsModel> GetPlaylistsList()
+        //{
+        //    return new List<PlaylistsModel>()
+        //    {
+        //        new PlaylistsModel()
+        //        {
+        //            Title = ""
+        //        }
+        //    }
+        //}
+    }
+}
---
+++ domshyra/Models/PlaylistsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace domshyra.Models
+{
+    public class PlaylistsModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string AppleMusicLink { get; set; }
+        public string SpotifyMusicLink { get; set; }
+    }
+}
