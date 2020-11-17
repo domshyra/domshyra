37242b9519e4d7e3f361401a54ebd14221778974
domshyra <domshyra@gmail.com>
2020-11-17 13:06:55 -0800

    Added crossfade note

--- domshyra/Models/PlaylistsModel.cs
+++ domshyra/Models/PlaylistsModel.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace domshyra.Models
 {
@@ -13,6 +11,29 @@ namespace domshyra.Models
         public string SpotifyMusicLink { get; set; }
         public string ImageURL { get; set; }
         public string SpotifyId { get; set; }
+        [DisplayFormat(DataFormatString = "{0:n0}")]
         public string TrackCount { get; set; }
+        public string FollowerCount { get; set; }
+        public string TrackAndFollowerText
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(FollowerCount))
+                {
+                    return $"{TrackCount} tracks";
+                }
+                else
+                {
+                    return $"{TrackCount} tracks and {FollowerCount} followers";
+                }
+            }
+        }
+        public string CrossFadeText { get; }
+
+        public PlaylistsModel()
+        {
+            string helpURL = "https://support.spotify.com/us/article/crossfade-feature/";
+            CrossFadeText = $"For best radio experience use <a href='{helpURL}' title='Crossfade recommened at 6 seconds or more' aria-label='Crossfade from spotify recommened at 6 seconds or more' data-toggle='tooltip' data-placement='bottom'>Spotiy's crossfade feature</a>";
+        }
     }
 }
