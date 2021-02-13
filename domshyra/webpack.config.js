9d038552241907339c561a0b83ffedc44fb9f695
Dom Schira <domshyra@gmail.com>
2021-02-13 07:56:05 -0800

    fixed the infinite loop of WebPack

--- domshyra/webpack.config.js
+++ domshyra/webpack.config.js
@@ -56,7 +56,7 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: "./wwwroot/dist/index.html"
+            template: "./wwwroot/src/index.html"
         }),
         new MiniCssExtractPlugin({
             filename: "css/bundle.css" 
