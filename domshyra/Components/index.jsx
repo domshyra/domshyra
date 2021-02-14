391ab735da94c56913b45d40caed81f0f7e2f7ab
Dom Schira <domshyra@gmail.com>
2021-02-13 19:44:00 -0800

    Added bootstrap js to the mix

--- domshyra/Components/index.jsx
+++ domshyra/Components/index.jsx
@@ -1,5 +1,7 @@
 ﻿import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap"
+
 import HelloReact from "./HelloReact";
 
 ReactDOM.render(<HelloReact />, document.getElementById("hello-react"));
