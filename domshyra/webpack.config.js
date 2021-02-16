9a272b680d144645b76444e8d9d9971771dd6030
Dom Schira <domshyra@gmail.com>
2021-02-16 07:31:48 -0800

    seems like sass compiles but just not into css

--- domshyra/webpack.config.js
+++ domshyra/webpack.config.js
@@ -5,11 +5,11 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-    entry: "./Components/index.jsx", //where react is getting is elements to load into the DOM
+    //where react is getting is elements to load into the DOM
+    entry: "./Components/index.jsx", 
     devtool: 'source-map',
     module: {
         rules: [
-            //might need to have 'use: ["babel-loader", "eslint-loader"],' included but dunno yet
             {
                 test: /\.(js|jsx)$/,
                 exclude: /(node_modules)/,
