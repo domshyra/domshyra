2260d11e9b5a2dab83f74a0f9969f89e9b940672
Dom Schira <domshyra@gmail.com>
2021-03-04 12:42:55 -0800

    Reaact elements render but no styling

--- domshyra/Components/Radio.jsx
+++ domshyra/Components/Radio.jsx
@@ -1,10 +1,64 @@
 ﻿import React from "react";
 import ReactDOM from "react-dom";
+import RadioCard from "./RadioCard.jsx"
 
-const Radio = () => {
-    return (
-        <div>Hello World</div>
+const testData = [{
+    AnchorId: "ambient-album-radio",
+    AppleMusicLink: "https://music.apple.com/us/playlist/equanimity-radio/pl.u-GgN8RCbo4Mrl",
+    CrossFadeText: "For best radio experience use <a href='https://support.spotify.com/us/article/crossfade-feature/' title='Crossfade recommened at 6 seconds or more' aria-label='Crossfade from spotify recommened at 6 seconds or more' data-toggle='tooltip' data-placement='bottom'>Spotiy's crossfade feature</a>",
+    Description: "great background music. (afro beats, french pop, sahara psychedelic blues, jazz, soul, funk, instrumental rock, world)",
+    FollowerCount: 0,
+    ImageURL: "https://i.scdn.co/image/ab67706c0000bebb2bec5f2c5a3c4635d43c34f9",
+    SpotifyId: "5IUjuF00hzonDk6MzuanBs",
+    SpotifyMusicLink: "https://open.spotify.com/playlist/5IUjuF00hzonDk6MzuanBs",
+    Title: "Ambient Album Radio",
+    TrackAndFollowerText: "787 tracks",
+    TrackCount: 787
+}];
+
+const Radios = (props) => {
+
+
+    const radioItems = props.playlists.map((radio) =>
+        <RadioCard radio={radio} key={radio.AnchorId} />
     );
+    
+
+    return (radioItems);
 };
 
-ReactDOM.render(<Radio />, document.getElementById("radio-content"));
+ReactDOM.render(<Radios playlists={testData} />, document.getElementById("radio-content"));
+
+
+
+
+/*
+ 
+ import React from "react";
+import ReactDOM from "react-dom";
+import RadioCard from "./RadioCard.jsx"
+
+
+
+const Radios = (props) => {
+    global radioURL 
+eslint no-undef: "error"
+
+
+
+
+return (
+    fetch(radioURL)
+        .then(response => response.json())
+        .then(spotifyPlaylists =>
+            spotifyPlaylists.map((radio) =>
+                console.log(radio)
+            )
+        )
+);
+};
+
+ReactDOM.render(<Radios />, document.getElementById("radio-content"));
+
+
+ */
--- domshyra/Components/RadioCard.jsx
+++ domshyra/Components/RadioCard.jsx
@@ -1,1 +1,72 @@
-﻿
+﻿import React from "react";
+import PropTypes from "prop-types";
+
+const RadioCard = (props) => {
+    return (
+        <>
+            <CardElementDesktop radio={props} />
+            <CardElementMobile radio={props} />
+        </>
+    );
+};
+
+export default RadioCard;
+
+const CardElementDesktop = (props) => {
+    const radio = props.radio;
+    return (
+        <div className="d-none d-xl-block">
+            <div className="card mb-2">
+                <div className="row no-gutters">
+                    <div className="col-6">
+                        <img src={radio.ImageURL} className="card-img px-2 py-2 playlist-img" alt="..." />
+                    </div>
+                </div>
+                <div className="col-md-8 col-6">
+                    <CardBody details={props} />
+                </div>
+            </div>
+        </div>
+    );
+}
+CardElementDesktop.propTypes = {
+    ImageURL: PropTypes.string,
+    radio: PropTypes.object
+};
+const CardElementMobile = (props) => {
+    return (
+        <div className="d-xl-none">
+            <div className="card mb-3 ">
+                <img src={props.ImageURL} className="card-img-top px-3 pt-3" alt="..." />
+                <CardBody details={props} />
+            </div>
+        </div>
+    );
+}
+CardElementMobile.propTypes = {
+    ImageURL: PropTypes.string
+};
+
+
+const CardBody = (props) => {
+    const details = props.details;
+    return (
+        <div className="card-body">
+            <h5 className="card-title font-weight-bold">{details.Title}</h5>
+            <p className="card-text">{details.Description}</p>
+            <blockquote className="mb-0">
+                <p className="mb-0 font-weight-light">{details.TrackAndFollowerText}</p>
+                <footer><small className="text-muted font-weight-light">{details.CrossFadeText}</small></footer>
+            </blockquote>
+
+        </div>
+    );
+}
+
+CardBody.propTypes = {
+    Description: PropTypes.string,
+    details: PropTypes.object,
+    Title: PropTypes.string,
+    TrackAndFollowerText: PropTypes.string,
+    CrossFadeText: PropTypes.string
+};
