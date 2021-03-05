aa489abe0dc5169d61ca1e697cf6675f3d924375
Dom Schira <domshyra@gmail.com>
2021-03-04 17:09:42 -0800

    removed the test data

--- domshyra/Components/Radio.jsx
+++ domshyra/Components/Radio.jsx
@@ -3,21 +3,6 @@ import ReactDOM from "react-dom";
 import RadioCard from "./RadioCard.jsx"
 import PropTypes from "prop-types";
 
-
-const testData = [{
-    AnchorId: "ambient-album-radio",
-    AppleMusicLink: "https://music.apple.com/us/playlist/equanimity-radio/pl.u-GgN8RCbo4Mrl",
-    CrossFadeText: "For best radio experience use <a href='https://support.spotify.com/us/article/crossfade-feature/' title='Crossfade recommened at 6 seconds or more' aria-label='Crossfade from spotify recommened at 6 seconds or more' data-toggle='tooltip' data-placement='bottom'>Spotiy's crossfade feature</a>",
-    Description: "great background music. (afro beats, french pop, sahara psychedelic blues, jazz, soul, funk, instrumental rock, world)",
-    FollowerCount: 0,
-    ImageURL: "https://i.scdn.co/image/ab67706c0000bebb2bec5f2c5a3c4635d43c34f9",
-    SpotifyId: "5IUjuF00hzonDk6MzuanBs",
-    SpotifyMusicLink: "https://open.spotify.com/playlist/5IUjuF00hzonDk6MzuanBs",
-    Title: "Ambient Album Radio",
-    TrackAndFollowerText: "787 tracks",
-    TrackCount: 787
-}];
-
 const Radios = (props) => {
     const playlists = props.playlists;
     const radioItems = playlists.map((radio) =>
