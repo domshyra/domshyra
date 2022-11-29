93fe17a9a1928a55734c6caec7ed4679f412f6f5
Dominic Schira <DominicS@wsrb.com>
2022-11-29 11:45:20 -0800

    Got it lookin nice

--- Web/src/components/PlaylistCard.jsx
+++ Web/src/components/PlaylistCard.jsx
@@ -13,53 +13,6 @@ import Typography from "@mui/material/Typography";
 
 const PlaylistCard = ({ title, imageURL, description, genre, trackAndFollowerText }) => {
 	return (
-		<Card sx={{ maxWidth: 345 }}>
-			<CardActionArea>
-				<CardHeader
-					title={title}
-					// subheader={trackAndFollowerText}
-					action={
-						<IconButton aria-label="settings">
-							<LibraryAddIcon />
-						</IconButton>
-					}
-				/>
-				 <Box sx={{ width: 300, resize: 'horizontal', overflow: 'auto', p: 1 }}>
-      <AspectRatio minHeight={120} maxHeight={200} objectFit="contain">
-        <img
-          src={imageURL}
-          alt=""
-        />
-      </AspectRatio>
-    </Box>
-
-				<CardMedia
-					component="img"
-					//   height="140"
-					image={imageURL}
-                    sx={{ width: 151 }}
-					alt={`${title} image`}
-				/>
-				<CardContent>
-					<Typography variant="body1" color="text.secondary" gutterBottom>
-						{description}
-					</Typography>
-					<Typography variant="caption" color="text.secondary.light">
-						{genre}
-					</Typography>
-				</CardContent>
-			</CardActionArea>
-			<CardActions>
-				<Button size="small" color="primary">
-					Share
-				</Button>
-			</CardActions>
-		</Card>
-	);
-};
-
-const PlaylistCardV2 = ({ title, imageURL, description, genre, trackAndFollowerText }) => {
-	return (
 		<Card sx={{ display: "flex" }}>
 			<Box sx={{ display: "flex", flexDirection: "column", width: 215 }}>
 				<CardContent sx={{ flex: "1 0 auto" }}>
@@ -88,6 +41,40 @@ const PlaylistCardV2 = ({ title, imageURL, description, genre, trackAndFollowerT
 				</AspectRatio>
 			</Box>
 			{/* <CardMedia component="img" sx={{ width: 200 }} image={imageURL} alt={`${title} image`} /> */}
+		</Card>
+	);
+};
+
+const PlaylistCardV2 = ({ title, imageURL, description, genre, trackAndFollowerText }) => {
+	return (
+		<Card sx={{ display: "flex", maxWidth: 430, minHeight: 150 }}>
+			<Box sx={{ display: "flex", flexDirection: "column" }}>
+				<CardContent sx={{ flex: "1 0 auto", width:215 }}>
+					<Typography component="div" variant="h6">
+						{title}
+					</Typography>
+					<Typography variant="subtitle2" color="text.secondary" component="div" gutterBottom>
+						{description}
+					</Typography>
+
+				</CardContent>
+				<Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1, maxWidth:215 }}>
+					<Tooltip title={genre}>
+					<IconButton aria-label="genre">
+						<QueueMusicIcon />
+					</IconButton>
+					</Tooltip>
+					<Typography variant="caption" color="text.secondary.light" noWrap>
+						{genre}
+					</Typography>
+				</Box>
+			</Box>
+			{/* <Box sx={{ width: 215, overflow: "auto", p: 1, borderRadius: 'md' }}>
+				<AspectRatio ratio="4/3" minWidth={120} maxWidth={200} objectFit="contain">
+					<img src={imageURL} alt="" />
+				</AspectRatio>
+			</Box> */}
+			<CardMedia component={AspectRatio} ratio="4/3" objectFit="contain" sx={{ width: 215 }} image={imageURL} alt={`${title} image`} />
 		</Card>
 	);
 };
