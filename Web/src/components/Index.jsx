1c62b190727e47facab0c6809dc553b22ef1753c
Dominic Schira <DominicS@wsrb.com>
2022-11-28 17:17:48 -0800

    Added cards

--- Web/src/components/Index.jsx
+++ Web/src/components/Index.jsx
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from "react";
 
+import { Grid } from "@mui/material";
+import PlaylistCard from "./PlaylistCard";
 import { useGetPlaylistsQuery } from "../redux/services/spotifyApi";
 
 const Index = () => {
 
 	const { data, isLoading } = useGetPlaylistsQuery();
-	const [playlists, setPlaylists] = useState([])
+	const [playlists, setPlaylists] = useState([]);
+	const [cards, setCards] = useState([]);
 
 	  useEffect(() => {
 			if (!isLoading && data) {
 				setPlaylists(data);
+				setCards(renderCards(data));
 			}
 		}, [data, isLoading]);
+
+	
 
 	const rows = (data) => {
 		if (!data) {
@@ -27,8 +33,15 @@ const Index = () => {
 		));
 	};
 
+
+	const renderCards = (data) => {
+		return data.map((item) => (
+			<PlaylistCard {...item} key={item.spotifyId}/>
+		));
+	}
+
 	return (
-		<div>
+		<Grid mt={4}>
 			<table className="table table-hover">
 				<thead>
 					<tr>
@@ -42,7 +55,9 @@ const Index = () => {
 					{rows(playlists)}
 				</tbody>
 			</table>
-		</div>
+
+			{!isLoading ? cards : null}
+		</Grid>
 	);
 };
 
---
+++ Web/src/components/PlaylistCard.jsx
@@ -0,0 +1,60 @@
+import * as React from "react";
+
+import { Button, CardActionArea, CardActions, CardHeader, IconButton } from "@mui/material";
+
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import LibraryAddIcon from '@mui/icons-material/LibraryAdd';
+import { PropTypes } from "prop-types";
+import Typography from "@mui/material/Typography";
+
+const PlaylistCard = ({ title, imageURL, description, genre, trackAndFollowerText }) => {
+	return (
+		<Card sx={{ maxWidth: 345 }}>
+			<CardActionArea>
+				<CardHeader
+					title={title}
+					// subheader={trackAndFollowerText}
+					action={
+						<IconButton aria-label="settings">
+							<LibraryAddIcon />
+						</IconButton>
+					}
+				/>
+				<CardMedia
+					component="img"
+					//   height="140"
+					image={imageURL}
+                    // sx={{ width: 151 }}
+					alt={`${title} image`}
+				/>
+				<CardContent>
+					<Typography variant="body1" color="text.secondary" gutterBottom>
+						{description}
+					</Typography>
+					<Typography variant="caption" color="text.secondary.light">
+						{genre}
+					</Typography>
+				</CardContent>
+			</CardActionArea>
+			<CardActions>
+				<Button size="small" color="primary">
+					Share
+				</Button>
+			</CardActions>
+		</Card>
+	);
+};
+PlaylistCard.propType = {
+	title: PropTypes.string,
+	anchorId: PropTypes.string,
+	description: PropTypes.string,
+	genre: PropTypes.string,
+	spotifyMusicLink: PropTypes.string,
+	imageURL: PropTypes.string,
+	spotifyId: PropTypes.string,
+	trackAndFollowerText: PropTypes.string,
+};
+
+export default PlaylistCard;
