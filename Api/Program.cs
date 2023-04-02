3966da55119d0a20e24fdeaf45027973beb5f11f
domshyra <domshyra@gmail.com>
2023-04-02 11:58:00 -0700

    Orginization

--- Api/Program.cs
+++ Api/Program.cs
@@ -29,46 +29,9 @@ if (app.Environment.IsDevelopment())
 app.UseStaticFiles();
 app.UseHttpsRedirection();
 
-app.MapGet("/spotify", async (ISpotifyProvider _spotifyProvider) =>
-{
-    return await _spotifyProvider.GetPlaylists();
-}).WithName("GetSpotifyPlaylists");
-
-app.MapGet("/spotify/{playlistId}", async (string playlistId, ISpotifyProvider _spotifyProvider) =>
-{
-    return await _spotifyProvider.GetPlaylist(playlistId);
-}).WithName("GetSpotifyPlaylist");
-
-app.MapGet("/ratings", (IPlaylistRepo repo) => repo.GetRatings()).Produces<PlaylistRatingDto[]>(StatusCodes.Status200OK);
-app.MapGet("/ratings/{playlistId}", async (string playlistId, IPlaylistRepo repo) =>
-{
-    var rating = await repo.GetRating(playlistId);
-    if (rating == null)
-        return Results.NoContent();
-    return Results.Ok(rating);
-}).Produces<PlaylistRatingDto>(StatusCodes.Status200OK).Produces(StatusCodes.Status204NoContent);
-
-app.MapPost("/ratings/{playlistId}", async (string playlistId, [FromBody] int rating, IPlaylistRepo repo) =>
-{
-    var newRating = await repo.AddRating(playlistId, rating);
-    return Results.Created($"/ratings/{newRating.Id}", newRating);
-}).Produces<PlaylistRatingDto>(StatusCodes.Status201Created);
-
-app.MapPut("/ratings/{playlistId}", async (string playlistId, [FromBody] int rating, IPlaylistRepo repo) =>
-{
-    var existingRating = await repo.GetRating(playlistId);
-    if (existingRating == null)
-        return Results.NoContent();
-    var updatedRating = await repo.UpdateRating(playlistId, rating);
-    return Results.Ok(updatedRating);
-}).Produces<PlaylistRatingDto>(StatusCodes.Status200OK)
-    .Produces(StatusCodes.Status204NoContent);
-
-app.MapDelete("/ratings/{id}", async (string id, IPlaylistRepo repo) =>
-{
-    await repo.DeleteRating(id);
-    return Results.Ok();
-}).Produces(StatusCodes.Status200OK);
+//Routes
+UseSpotifyPlaylistRoutes(app);
+UseRatingsRoutes(app);
 
 
 //TODO remove
@@ -79,3 +42,50 @@ app.MapFallbackToFile("index.html");
 
 
 app.Run();
+
+static void UseSpotifyPlaylistRoutes(WebApplication app)
+{
+    app.MapGet("/spotify", async (ISpotifyProvider _spotifyProvider) =>
+    {
+        return await _spotifyProvider.GetPlaylists();
+    }).WithName("GetSpotifyPlaylists");
+
+    app.MapGet("/spotify/{playlistId}", async (string playlistId, ISpotifyProvider _spotifyProvider) =>
+    {
+        return await _spotifyProvider.GetPlaylist(playlistId);
+    }).WithName("GetSpotifyPlaylist");
+}
+
+static void UseRatingsRoutes(WebApplication app)
+{
+    app.MapGet("/ratings", (IPlaylistRepo repo) => repo.GetRatings()).Produces<PlaylistRatingDto[]>(StatusCodes.Status200OK);
+    app.MapGet("/ratings/{playlistId}", async (string playlistId, IPlaylistRepo repo) =>
+    {
+        var rating = await repo.GetRating(playlistId);
+        if (rating == null)
+            return Results.NoContent();
+        return Results.Ok(rating);
+    }).Produces<PlaylistRatingDto>(StatusCodes.Status200OK).Produces(StatusCodes.Status204NoContent);
+
+    app.MapPost("/ratings/{playlistId}", async (string playlistId, [FromBody] int rating, IPlaylistRepo repo) =>
+    {
+        var newRating = await repo.AddRating(playlistId, rating);
+        return Results.Created($"/ratings/{newRating.Id}", newRating);
+    }).Produces<PlaylistRatingDto>(StatusCodes.Status201Created);
+
+    app.MapPut("/ratings/{playlistId}", async (string playlistId, [FromBody] int rating, IPlaylistRepo repo) =>
+    {
+        var existingRating = await repo.GetRating(playlistId);
+        if (existingRating == null)
+            return Results.NoContent();
+        var updatedRating = await repo.UpdateRating(playlistId, rating);
+        return Results.Ok(updatedRating);
+    }).Produces<PlaylistRatingDto>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status204NoContent);
+
+    app.MapDelete("/ratings/{id}", async (string id, IPlaylistRepo repo) =>
+    {
+        await repo.DeleteRating(id);
+        return Results.Ok();
+    }).Produces(StatusCodes.Status200OK);
+}
