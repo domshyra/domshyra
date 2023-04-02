96d138589809e2ba40fd4e9611ddbb976deadd9a
domshyra <domshyra@gmail.com>
2023-04-02 14:40:49 -0700

    remove cors for now

--- Api/Program.cs
+++ Api/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllersWithViews();
 //TODO remove
-builder.Services.AddCors();
+// builder.Services.AddCors();
 builder.Services.AddDbContext<PlaylistDbContext>(options =>
     options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
 
@@ -35,7 +35,7 @@ UseRatingsRoutes(app);
 
 
 //TODO remove
-app.UseCors(p => p.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod());
+// app.UseCors(p => p.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod());
 app.UseRouting();
 app.UseEndpoints(e => e.MapDefaultControllerRoute());
 app.MapFallbackToFile("index.html");
