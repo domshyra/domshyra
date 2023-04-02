22fb745c4b9843d5fa985278db98338c4c76af78
domshyra <domshyra@gmail.com>
2023-04-02 14:34:29 -0700

    removing dev for swagger for now

--- Api/Program.cs
+++ Api/Program.cs
@@ -21,11 +21,11 @@ builder.Services.AddScoped<IPlaylistRepo, PlaylistRepo>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+// }
 app.UseStaticFiles();
 app.UseHttpsRedirection();
 
