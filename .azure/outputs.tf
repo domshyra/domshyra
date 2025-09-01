0aba56cf664164913b5354b9c756ba334cfb476f
domshyra <domshyra@gmail.com>
2025-09-01 13:19:07 -0400

    more tf improvments
    fix for the dns binding

--- .azure/outputs.tf
+++ .azure/outputs.tf
@@ -1,22 +1,22 @@
 output "app_service_publish_profile_api" {
-  value = azurerm_windows_web_app.repository_name["api"].id
+  value = azurerm_windows_web_app.domshyra["api"].id
 }
 
 output "app_service_publish_profile_web" {
-  value = azurerm_windows_web_app.repository_name["web"].id
+  value = azurerm_windows_web_app.domshyra["web"].id
 }
 
 output "app_service_client_id_api" {
-  value = azurerm_user_assigned_identity.repository_name["api"].principal_id
+  value = azurerm_user_assigned_identity.domshyra["api"].principal_id
 }
 
 output "app_service_client_id_web" {
-  value = azurerm_user_assigned_identity.repository_name["web"].principal_id
+  value = azurerm_user_assigned_identity.domshyra["web"].principal_id
 }
 
 output "app_service_inbound_ip_api" {
-  value = azurerm_windows_web_app.repository_name["api"].outbound_ip_address_list[length(azurerm_windows_web_app.repository_name["api"].outbound_ip_address_list) - 1]
+  value = azurerm_windows_web_app.domshyra["api"].outbound_ip_address_list[length(azurerm_windows_web_app.domshyra["api"].outbound_ip_address_list) - 1]
 }
 output "app_service_inbound_ip_web" {
-  value = azurerm_windows_web_app.repository_name["web"].outbound_ip_address_list[length(azurerm_windows_web_app.repository_name["web"].outbound_ip_address_list) - 1]
+  value = azurerm_windows_web_app.domshyra["web"].outbound_ip_address_list[length(azurerm_windows_web_app.domshyra["web"].outbound_ip_address_list) - 1]
 }
