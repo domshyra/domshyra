a04dfa7d9cd026380da9cc0d85b8a946b6a364f4
Dominic Schira <domshyra@gmail.com>
2025-09-07 16:05:32 -0700

    removing unused stuff (#119)
    fix for import
    
    fixes

--- .azure/import.sh
+++ .azure/import.sh
@@ -4,10 +4,8 @@
 source .env.local
 
 # Load Terraform variables and initialize Terraform
-source local.sh && terraform init \
+source local.sh && terraform import resource_name.domshyra "/subscriptions/sub_id/resourceGroups/rg-domshyra/providers/Microsoft.KeyVault/vaults/kv-vault/objectId/obj_id" \
     -backend-config="container_name=${backend_container_name}" \
     -backend-config="storage_account_name=${backend_storage_account_name}" \
     -backend-config="key=terraform.tfstate" \
     -backend-config="resource_group_name=rg-tfstate" \
-    && echo "Terraform initialized successfully." \
-    && terraform import resource_name.domshyra "/subscriptions/sub_id/resourceGroups/rg-domshyra/providers/Microsoft.KeyVault/vaults/kv-vault/objectId/obj_id" \
