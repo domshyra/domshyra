27576a15a0722f772aebd9a0cb790fec67cd0f1b
domshyra <domshyra@gmail.com>
2025-09-21 15:38:29 -0400

    plan i s a bit beter

--- .azure/plan.sh
+++ .azure/plan.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # Load Terraform variables and plan changes
-source local.sh && terraform plan -out=tfplan
+source init.sh &&
+TF_LOG=DEBUG terraform plan -out=tfplan && echo "Terraform plan created successfully."
+if [ $? -ne 0 ]; then
+  echo "Terraform plan failed."
+fi
