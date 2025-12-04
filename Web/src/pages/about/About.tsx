cf2b20265fd6339e7bbc5713bf10febe716de900
domshyra <domshyra@gmail.com>
2025-12-03 20:50:57 -0500

    minor updates from resume

--- Web/src/pages/about/About.tsx
+++ Web/src/pages/about/About.tsx
@@ -1,10 +1,11 @@
-import { Box, Stack, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { Box, Divider, Link, Stack, Typography, useMediaQuery, useTheme } from "@mui/material";
 
+import Skills from "@pages/skills/Skills";
 import SocialButtons from "@components/socalis/SocialButtons";
+import WorkHistory from "@pages/work/WorkHistory";
 import { aboutMeExtendedPitch } from "@constants/data";
 import { aboutMeImgUrl } from "@constants/common";
 import { useNavigate } from "react-router-dom";
-import { work } from "@constants/routes";
 
 //TODO? use some animations for section transitions, like fade in or slide in
 //? https://mobbin.com/sites/retool-57b95056-1028-4d7b-a14a-c22f6c8694b6/65564b72-19e0-4bb7-b31e-288a72b5989a/preview
@@ -18,11 +19,11 @@ const About = () => {
 
 	const typographyStyle = { width: "90%", mx: { xs: 3, md: 20, lg: 40 }, whiteSpace: "pre-line", px: 0.5 };
 	const goToWork = () => {
-		navigate(`../${work}`);
+		window.open("https://domdeckard.com", "_blank", "noopener noreferrer");
 	};
 	return (
 		<>
-			<Typography textAlign="center" variant="h4" sx={{ width: "100%" }} pb={2}>
+			<Typography textAlign="center" variant="h3" sx={{ width: "100%" }} pb={2}>
 				About
 			</Typography>
 			{/* show on top for mobile */}
@@ -54,17 +55,20 @@ const About = () => {
 					<Typography
 						textAlign="center"
 						variant="body1"
-						color="primary"
+						color="text.secondary"
 						fontWeight={400}
 						sx={{
 							...typographyStyle,
-							cursor: "pointer",
-							underline: "hover",
 							mb: 1,
 						}}
-						onClick={goToWork}
 					>
-						View my work here.
+						I also create music under the moniker of Dom Deckard{" "}
+						<Link fontWeight={400} onClick={goToWork} target="_blank" rel="noopener" underline="none" sx={{ cursor: "pointer" }}>
+							here
+						</Link>
+					</Typography>
+					<Typography variant="caption" fontWeight={400} justifyContent="center" display="flex" sx={{ width: "100%" }}>
+						View my other coding profiles, and social links below
 					</Typography>
 					<Box sx={{ width: isMobile ? "100%" : "60%", margin: "0 auto", mb: 2, alignItems: "center" }}>
 						<SocialButtons />
@@ -85,6 +89,10 @@ const About = () => {
 				}}
 			/>
 
+			<Divider sx={{ color: "primary.main", pr: 10, mr: 10, my: 4, width: "100%" }} />
+			<WorkHistory />
+			<Divider sx={{ color: "primary.main", pr: 10, mr: 10, my: 4, width: "100%" }} />
+			<Skills />
 			{/* This will prob end up being it's own page, make sure to show responsibilities and what I did to make the company better. */}
 			{/* What was the most fun thing I could work on for each project? C&B was the tree's and what if for blocks */}
 			{/* templates and msal-selenium for wsrb, modernizing things and bringing the team new technologies and best practices, for many a things */}
