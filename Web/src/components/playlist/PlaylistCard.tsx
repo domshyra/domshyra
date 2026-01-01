0e19f69639f035cfd7a4102d6f7cef9fa1833be8
domshyra <domshyra@gmail.com>
2026-01-01 16:06:14 -0500

    good clean up, but minor fixes to follow

--- Web/src/components/playlist/PlaylistCard.tsx
+++ Web/src/components/playlist/PlaylistCard.tsx
@@ -1,14 +1,93 @@
-import PlaylistCardView from "./PlaylistCardView";
-import { playlist } from "@_types/playlist";
+import { Box, Button, Skeleton, Stack, Tooltip } from "@mui/material";
 
-type PlaylistCardProps = playlist & {
-	loading?: boolean;
-};
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import { PlaylistCardProps } from "./PlaylistCardProps";
+import PlaylistPhoto from "@fragments/playlistPhoto/PlaylistPhoto";
+import SpotifyLink from "@fragments/spotify/SpotifyLink";
+import Typography from "@mui/material/Typography";
+import { stations } from "@constants/routes";
+import { useNavigate } from "react-router-dom";
 
-const PlaylistCard = (props: PlaylistCardProps) => {
+const PlaylistCard = ({
+	title,
+	imageURL,
+	description,
+	genre,
+	trackAndFollowerText,
+	playlistId,
+	loading,
+	trackCount,
+	followerCount,
+	isDetailsPage,
+}: PlaylistCardProps) => {
 	const sectionWidth = 215;
 	const cardWidth = sectionWidth * 2;
-	return <PlaylistCardView {...props} sectionWidth={sectionWidth} cardWidth={cardWidth} />;
+	const nav = useNavigate();
+	const trackCountFollowerCountText = trackAndFollowerText
+		? trackAndFollowerText
+		: loading
+			? ""
+			: `${trackCount ?? 0} tracks${followerCount ? `, ${followerCount} followers` : ""}`;
+	return (
+		<Card sx={{ width: cardWidth, minHeight: 200 }} className="Cardbk">
+			<Stack direction={{ xs: "column", md: isDetailsPage ? "column" : "row" }}>
+				<Box sx={{ display: "flex", flexDirection: "column", width: { xs: "100%", md: isDetailsPage ? "100%" : "50%" } }}>
+					<CardContent sx={{ flex: "1 0 auto" }}>
+						<Typography
+							sx={{ cursor: "pointer" }}
+							component="div"
+							variant="h5"
+							color="primary"
+							onClick={() => nav(`${stations}/${playlistId}`, { state: { title } })} // Navigate to playlist details
+						>
+							{loading ? <Skeleton /> : title}
+						</Typography>
+						<Typography variant="subtitle2" color="text.secondary" component="div">
+							{loading ? <Skeleton width="100%" /> : description}
+						</Typography>
+						<Typography component="div" variant="caption" color="text.secondary" fontWeight="light" flexWrap="wrap">
+							{loading ? <Skeleton /> : genre}
+						</Typography>
+					</CardContent>
+				</Box>
+				<Box sx={{ display: "block", width: { xs: "100%", md: isDetailsPage ? "100%" : "50%" } }}>
+					<PlaylistPhoto imageURL={imageURL} title={title} loading={loading} />
+				</Box>
+			</Stack>
+			<Box sx={{ display: "flex", flexDirection: "column" }}>
+				<Box sx={{ display: { xs: "none", md: "flex" }, alignItems: "center", justifyContent: "space-between", px: 1, width: "100%" }}>
+					<Tooltip title={genre} placement="bottom-start" arrow>
+						<Button aria-label="genre" color="secondary" size="small" startIcon={<MusicNoteIcon />}>
+							Genre
+						</Button>
+					</Tooltip>
+					<Typography variant="caption" color="text.secondary.light" noWrap align="right">
+						{loading ? <Skeleton width={25} /> : trackCountFollowerCountText}
+					</Typography>
+					<SpotifyLink playlistId={playlistId} loading={loading ?? false} />
+				</Box>
+				<Box sx={{ display: { xs: "block", md: "none" }, alignItems: "center", width: "100%" }}>
+					<Box
+						sx={{
+							display: "flex",
+							alignItems: "center",
+							justifyContent: "space-between",
+							pl: 1,
+							pr: 2,
+							width: "100%",
+						}}
+					>
+						<Typography variant="caption" color="text.secondary.light" noWrap align="left">
+							{loading ? <Skeleton width={25} /> : trackCountFollowerCountText}
+						</Typography>
+						<SpotifyLink playlistId={playlistId} loading={loading ?? false} />
+					</Box>
+				</Box>
+			</Box>
+		</Card>
+	);
 };
 
 export default PlaylistCard;
--- Web/src/fragments/spotify/SpotifyLink.tsx
+++ Web/src/fragments/spotify/SpotifyLink.tsx
@@ -2,7 +2,7 @@ import { Link, Typography } from "@mui/material";
 
 const SpotifyLink = ({ playlistId, loading }: { playlistId: string; loading: boolean }) => {
 	return (
-		<Typography variant="subtitle2" color="text.secondary" component="div" gutterBottom>
+		<Typography variant="subtitle2" color="text.secondary" textAlign="center">
 			<Link rel="noopener" target="_blank" href={`https://open.spotify.com/playlist/${playlistId}`} underline="hover">
 				{loading ? "Loading..." : "Open in Spotify"}
 			</Link>
